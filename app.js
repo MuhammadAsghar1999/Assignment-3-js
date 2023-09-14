332a0e6f5638f727e3070f87cb5c775d2301e046
MuhammadAsghar1999 <143824430+MuhammadAsghar1999@users.noreply.github.com>
2023-09-15 00:40:31 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,103 @@
+// Q:1
+// var arr =[]
+// arr.push("asghar",23,"hassan","mudassir")
+// console.log(arr)
+
+
+// Q:2
+
+
+
+
+// Q:3
+// var arr =[]
+// arr.push("asghar","hassan","mudassir")
+// console.log(arr)
+
+// Q:4
+// var arr =[]
+// arr.push(22,25,34)
+// console.<log(arr)
+
+
+// Q:5
+// var arr =[]
+// arr.push(true,false)
+// console.log(arr)
+
+// Q:6
+// var arr =[]
+// arr.push("asghar",23,"hassan","mudassir",true)
+// console.log(arr)
+
+// Q:7
+// var Qualifications =["SSC","HSC","BCS","BS","BCOM","MS"]
+//  document.write("<b> Qualifications </b> <br> <br>")
+//  document.write(Qualifications [0] + "<br>")
+//  document.write(Qualifications [1] + "<br>")
+//  document.write(Qualifications [3] + "<br>")
+//  document.write(Qualifications [4] + "<br>")
+//  document.write(Qualifications [5] + "<br>")
+
+// // Q:8
+// var students = ["asghar","hassan","Mudasssir"]
+// var score = [320,490,480]
+
+// var totalscore = [500,500,500]
+// document.write("score of " + students[0] + " is "+score[0] + "."+ " percentage "+(score[0]/totalscore[0])*100+"%<br>")
+// document.write("score of " + students[1] + " is "+score[1] + "."+ " percentage "+(score[1]/totalscore[1])*100+"%<br>")
+// document.write("score of " + students[2] + " is "+score[2] + "."+ " percentage "+(score[2]/totalscore[2])*100+"%<br>") 
+
+// Q:9
+
+// var colour = ["Red","blue","grey","brown"]
+// alert(colour [0])
+// alert(colour [1])
+// alert(colour [2])
+// alert(colour [3])
+
+// Q:9 (A)
+
+// var colour = ["Red","blue","grey","brown","pink"]
+// var newColor = prompt("Enter a color to add to the beginning of the array:");
+// colorNames.unshift("purple");
+// alert(colorNames);
+
+// Q:9 (B)
+
+// var color = ["Red", "Green", "Blue", "Yellow", "Orange"];
+// const newColor = prompt("Enter a color to add to the end of the array:");
+// colorNames.push("purple");
+// console.log(colorNames);
+
+
+// Q:9 (C)
+// var colour = ["Red","blue","grey","brown","pink"]
+// var newColor = prompt("Enter a color to add to the beginning of the array:");
+// colorNames.unshift("purple","black");
+// alert(colorNames);
+
+// Q:9 (D)
+// const colorNames = ["Red", "Green", "Blue", "Yellow", "Orange"];
+// colorNames.shift();
+// console.log(colorNames);
+
+// Q:9 (E)
+
+// const colorNames = ["Red", "Green", "Blue", "Yellow", "Orange"]
+// colorNames.pop();
+// console.log(colorNames);
+
+// Q:9 (F)
+
+// const colorNames = ["Red", "Green", "Blue", "Yellow", "Orange"];
+// const indexToAdd = parseInt(prompt("Enter the index where you want to add a color:"));
+// const colorToAdd = prompt("Enter the color name to add:");
+// if (indexToAdd >= 0 && indexToAdd <= colorNames.length) {
+//     colorNames.splice(indexToAdd, 0, colorToAdd)
+//     console.log(colorNames);
+// } else {
+//     console.log("Invalid index. The color was not added.");
+// }
+
+
